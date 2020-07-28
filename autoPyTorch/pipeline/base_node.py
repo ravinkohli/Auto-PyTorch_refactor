aa2824cb0e7b4c5c94d1ecdb398283602cbd990c
chico <francisco.rivera.valverde@gmail.com>
2020-07-28 13:48:08 +0200

    Initial idea of files

---
+++ autoPyTorch/pipeline/base_node.py
@@ -0,0 +1,28 @@
+from abc import ABCMeta
+
+import numpy as np
+from ConfigSpace import Configuration
+from sklearn.pipeline import Pipeline
+from sklearn.utils.validation import check_random_state
+
+from sklearn.base import BaseEstimator
+
+
+class AutoPytorchNode(BaseEstimator):
+    @staticmethod
+    def get_properties():
+        raise NotImplementedError()
+
+    @staticmethod
+    def get_hyperparameter_search_space():
+        raise NotImplementedError()
+
+    def fit(self, X, y):
+        raise NotImplementedError()
+
+    def set_hyperparameters(self, configuration, init_params=None):
+        raise NotImplementedError()
+
+    def __str__(self):
+        raise NotImplementedError()
+
