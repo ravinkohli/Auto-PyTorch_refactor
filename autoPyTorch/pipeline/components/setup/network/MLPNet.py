a9f1b8d8cf0e47da2b4a3b9ffbcd8f9887dbea22
chico <francisco.rivera.valverde@gmail.com>
2020-08-16 18:47:19 +0200

    flake fix

--- autoPyTorch/pipeline/components/setup/network/MLPNet.py
+++ autoPyTorch/pipeline/components/setup/network/MLPNet.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional
 
 import ConfigSpace as CS
 from ConfigSpace.configuration_space import ConfigurationSpace
