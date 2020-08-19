0f5c223ffac3c21c974f2b76559b2fc0421ef24c
Arlind Kadra <arlindkadra@gmail.com>
2020-08-19 13:55:37 +0200

    Initial commit with the structure

---
+++ autoPyTorch/search_space/search_space.py
@@ -0,0 +1,19 @@
+class SearchSpace:
+
+    def add_hyperparameter(
+        self,
+        upper,
+        lower,
+        log,
+        default,
+        hp_type,
+    ):
+        pass
+
+    def add_condition(
+        self,
+        hp1,
+        hp2,
+        cond,
+    ):
+        pass
