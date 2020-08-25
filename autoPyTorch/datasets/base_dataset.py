6f50acf2c49377e6b62eed325bf38f8910c2a5b4
Sebastian Walter <sebastian.walter98@gmail.com>
2020-08-25 11:40:53 +0200

    cleaned up cross validation and valdiation split functionality

--- autoPyTorch/datasets/base_dataset.py
+++ autoPyTorch/datasets/base_dataset.py
@@ -1,13 +1,14 @@
 from abc import ABCMeta
 from torch.utils import data
 import numpy as np
-from typing import Optional, Tuple, List
+from typing import Optional, Tuple, List, Dict, Callable
 
 
 class BaseDataset(data.Dataset, metaclass=ABCMeta):
     def __init__(self, train_tensors: Tuple[np.ndarray, ...], val_tensors: Optional[Tuple[np.ndarray, ...]] = None):
         self.train_tensors = train_tensors
         self.val_tensors = val_tensors
+        self.cross_validators = {}  # type: Dict[str, Callable[[int, np.ndarray], List[Tuple[np.ndarray, np.ndarray]]]]
 
     def __getitem__(self, index) -> Tuple[np.ndarray, ...]:
         return tuple(t[index] for t in self.train_tensors)
@@ -15,26 +16,22 @@ class BaseDataset(data.Dataset, metaclass=ABCMeta):
     def __len__(self) -> int:
         return self.train_tensors[0].shape[0]
 
+    def _get_data_indices(self) -> np.ndarray:
+        raise NotImplementedError
+
     def create_cross_val_splits(self, cross_val_type: str, num_splits: int) -> List[Tuple[data.Dataset, data.Dataset]]:
-        indices = np.random.permutation(len(self))
-        if cross_val_type == 'k-fold-crossvalidation':
-            splits = []
-            borders = list(range(0, len(self), (len(self) + num_splits - 1) // num_splits)) + [
-                len(self)]  # last split is smaller if len(self)
-            for i in range(len(borders) - 1):
-                lb, ub = borders[i], borders[i + 1]
-                splits.append((data.Subset(self, np.concatenate((indices[:lb], indices[ub:]))),
-                               data.Subset(self, indices[lb:ub])))
-        else:
-            NotImplementedError(f'The selected `cross_val_type` "{cross_val_type}" is not implemented.')
-        return splits
+        indices = self._get_data_indices()
+        if cross_val_type not in self.cross_validators:
+            raise NotImplementedError(f'The selected `cross_val_type` "{cross_val_type}" is not implemented.')
+        splits = self.cross_validators[cross_val_type](num_splits, indices)
+        return [(data.Subset(self, split[0]), data.Subset(self, split[1])) for split in splits]
 
     def create_val_split(self, val_share: float = None) -> Tuple[data.Dataset, data.Dataset]:
         if val_share is not None:
             if self.val_tensors is not None:
                 raise ValueError(
                     '`val_share` specified, but the Dataset was a given a pre-defined split at initialization already.')
-            indices = np.random.permutation(len(self))
+            indices = self._get_data_indices()
             val_count = round(val_share * len(self))
             return data.Subset(self, indices[:val_count]), data.Subset(self, indices[val_count:])
         else:
--- autoPyTorch/datasets/time_series_dataset.py
+++ autoPyTorch/datasets/time_series_dataset.py
@@ -1,7 +1,8 @@
 from torch.utils import data
 import numpy as np
 from autoPyTorch.datasets.base_dataset import BaseDataset
-from typing import Tuple, Optional, List, Set
+from typing import Tuple, Optional, Set
+from .cross_validation import time_series_cross_validation, k_fold_cross_validation
 
 
 class TimeSeriesForecastingDataset(BaseDataset):
@@ -29,30 +30,11 @@ class TimeSeriesForecastingDataset(BaseDataset):
                                                       target_variables=target_variables,
                                                       sequence_length=sequence_length,
                                                       n_steps=n_steps) if val_tensors is not None else None
+        self.cross_validators["time_series_cross_validation"] = time_series_cross_validation
         super().__init__(train_tensors=train, val_tensors=val)
 
-    def create_cross_val_splits(self, cross_val_type: str, num_splits: int) -> List[Tuple[data.Dataset, data.Dataset]]:
-        if self.val_tensors is not None:
-            raise ValueError(f"Cannot specify validation data when using cross validation.")
-        if cross_val_type == "time-series-fold":
-            splits = _time_series_cross_validation(self.train_tensors[0], num_splits)
-            return [(data.Subset(self, split[0]), data.Subset(self, split[1])) for split in splits]
-        else:
-            raise ValueError(f"Unknown cross validation type {cross_val_type}")
-
-    def create_val_split(self, val_share: float = None) -> Tuple[data.Dataset, data.Dataset]:
-        if val_share is not None:
-            if self.val_tensors is not None:
-                raise ValueError(
-                    '`val_share` specified, but the Dataset was a given a pre-defined split at initialization already.')
-            indices = np.arange(len(self))
-            val_count = round(val_share * len(self))
-            return data.Subset(self, indices[:val_count]), data.Subset(self, indices[val_count:])
-        else:
-            if self.val_tensors is None:
-                raise ValueError('Please specify `val_share` or initialize with a validation dataset.')
-            val_ds = BaseDataset(self.val_tensors)
-            return self, val_ds
+    def _get_data_indices(self) -> np.ndarray:
+        return np.arange(len(self))
 
 
 def _check_time_series_forecasting_inputs(target_variables: Set[int], sequence_length: int, n_steps: int,
@@ -102,39 +84,14 @@ def _prepare_time_series_forecasting_tensor(tensor: np.ndarray,
     return x_tensor, y_tensor
 
 
-def _time_series_cross_validation(tensor: np.ndarray, n_splits: int) -> List[Tuple[np.ndarray, np.ndarray]]:
-    """
-    Returns train and validation indices respecting the temporal ordering of the data.
-    Assumes the data is ordered ascending by time.
-    Assumes data format is NxSxM (N records of sequence length S with M features).
-    Dummy example: [0, 1, 2, 3] with 3 folds yields
-        [0] [1]
-        [0, 1] [2]
-        [0, 1, 2] [3]
-
-    :param tensor: tensor holding time series data, expected to be in format NxSxM
-    :param n_splits: number of cross validation splits
-    :return: list of tuples of training and validation indices
-    """
-    assert (tensor.ndim == 3)
-
-    split_size = int(np.ceil(tensor.shape[0] / (n_splits + 1)))
-
-    splits = []
-    train_bound = split_size
-    for i in range(n_splits):
-        train_indices = np.arange(min(train_bound, tensor.shape[0]))
-        test_indices = np.arange(train_bound, min(
-            train_bound + split_size, tensor.shape[0]))
-        splits.append((train_indices, test_indices))
-        train_bound += split_size
-    return splits
-
-
 class TimeSeriesClassificationDataset(BaseDataset):
     def __init__(self, train_tensors: Tuple[np.ndarray, ...], val_tensors: Optional[Tuple[np.ndarray, ...]] = None):
         _check_time_series_classification_inputs(train_tensors=train_tensors, val_tensors=val_tensors)
+        self.cross_validators["k_fold_cross_validation"] = k_fold_cross_validation
         super().__init__(train_tensors, val_tensors)
+
+    def _get_data_indices(self) -> np.ndarray:
+        return np.random.permutation(len(self))
 
 
 def _check_time_series_classification_inputs(train_tensors: Tuple[np.ndarray, ...],
